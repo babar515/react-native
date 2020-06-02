59ed00a64551dc1ea4283f4345ee289bb04c2791
babar <babar.shakeel515@gmail.com>
2020-06-02 20:02:10 +0500

    Counter Code myExpoApp

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React ,  { useState } from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+const App = props => {
+let [counter , setCounter] = useState(0);
+let [status , setStatus] = useState(false);
+let [check , setCheck] = useState(false);
+if (status)
+{
+ 
+setTimeout(()=>{
+setCounter(counter+1);
+},1000)
 }
+/* else
+{
+  clearTimeout(check)
+} */
+
+
+return(
+  <View style = {styles.container}>
+    <Text style={styles.text}>Counter  {counter}</Text>
+    
+ 
+  <View style = {styles.btncontainer}>
+
+  
+
+    <Button disabled = {status} title = 'Start' onPress = {() => {
+      setStatus(true)
+      setCheck(false);
+    } } />
+    <Button disabled = {!status} title = 'Stop' onPress = {() => {
+      setStatus(false),
+      setCheck(true);
+    } } />
+    <Button disabled = {!check} title = 'Reset' onPress = {() => {
+      setCounter(0) 
+      setCheck(false);
+    } } />
+  </View>
+
+  </View>
+)
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +54,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{color:'red',fontSize:25},
+  btncontainer:{flexDirection:'row'}
 });
+
+
+export default App;
